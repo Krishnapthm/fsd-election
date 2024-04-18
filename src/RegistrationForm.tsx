ce77967d994712879c50f55cbcd30e1cc43e020e
Krishnapthm <rajamanieta77@gmail.com>
2024-04-18 13:35:22 +0530

    added tailwind

---
+++ src/RegistrationForm.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function RegistrationForm() {
+    return (
+        <div className="text-red-500">
+            hello worldfsdsd
+        </div>
+    )
+};
